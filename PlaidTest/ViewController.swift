38fd18130beb46c01975c6ee7d6a1f881de41c52
Otto Suess <ottosuess@protonmail.com>
2021-10-07 15:31:10 +0200

    add LinkKit

--- PlaidTest/ViewController.swift
+++ PlaidTest/ViewController.swift
@@ -6,14 +6,25 @@
 //
 
 import UIKit
+import LinkKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var textField: UITextField!
+    private var linkHandler: Handler?
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction private func presentPlaid() {
+        guard let linkToken = textField.text else { return }
+
+        let configuration = LinkTokenConfiguration(token: linkToken) { _ in }
+        
+        let result = Plaid.create(configuration)
+        switch result {
+        case .failure(let error):
+            print(error)
+        case .success(let handler):
+            handler.open(presentUsing: .viewController(self))
+            linkHandler = handler
+        }
     }
-
-
 }
 
